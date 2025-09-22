bdda31cb6166bc27b13576a45def6411ee2e0bab
haticekatranciakgul <katrancihatice@icloud.com>
2025-09-22 16:48:48 +0300

    Install tools and lib

---
+++ social-links/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('@tailwindcss/postcss').Config} */
+const config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config
