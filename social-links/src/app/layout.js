07e443aec9595321c9454b28411f5fc8854b76d8
haticekatranciakgul <katrancihatice@icloud.com>
2025-09-24 23:20:10 +0300

    Update folder structer

--- social-links/src/app/layout.js
+++ social-links/src/app/layout.js
@@ -1,6 +1,6 @@
 "use client";
 import { Provider } from 'react-redux';
-import { store } from '../store/store';
+import { store } from '../store/index';
 import { AuthProvider } from '../contexts/AuthContext';
 import './globals.css';
 
